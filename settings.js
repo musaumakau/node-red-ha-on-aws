08f4a152f787361921697a5818d003fc5c44316c
Juan Musau <46565239+musaumakau@users.noreply.github.com>
2023-05-14 22:49:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -85,7 +85,7 @@ module.exports = {
 
     // By default, all user data is stored in the Node-RED install directory. To
     // use a different location, the following property can be used
-    userDir: process.env.EFS_MOUNT_DIR + '/.node-red/',
+//     userDir: process.env.EFS_MOUNT_DIR + '/.node-red/',
 
     // Node-RED scans the `nodes` directory in the install directory to find nodes.
     // The following property can be used to specify an additional directory to scan.
